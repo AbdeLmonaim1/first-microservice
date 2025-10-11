bbdde975a08c94642bf3ffb283445a8fe7e8e731
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-10-11 21:13:17 +0100

    use a generic REST web service that works with any entity

--- src/main/java/ma/enset/bankaccountservice/repositories/BankAccountRepository.java
+++ src/main/java/ma/enset/bankaccountservice/repositories/BankAccountRepository.java
@@ -2,6 +2,8 @@ package ma.enset.bankaccountservice.repositories;
 
 import ma.enset.bankaccountservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 }
